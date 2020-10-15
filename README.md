7495472aea91909505312ed2e9c0ed58086a666c
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-15 22:42:46 +0000

    Built out environment and run files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Finder Droid
+Hello!
---
+++ environment.rb
@@ -0,0 +1,8 @@
+require_relative "./lib/api"
+require_relative "./lib/cli"
+require_relative "./lib/film"
+require_relative "./lib/character"
+
+require 'pry'
+require 'net/http'
+require 'json'
