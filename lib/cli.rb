8cc747050f974c274456af3c7305ae0daa6966d3
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-21 05:32:18 +0000

    Began work on having program allow further films to be selected. Wanted more customized dialogue, will be building a new method

--- lib/cli.rb
+++ lib/cli.rb
@@ -15,6 +15,9 @@ class CLI
       puts "Allow me to display you a list of the films. One moment while I fetch them for you...".colorize(:light_blue)
       sleep(3)
       self.list_the_films
+      
+    else 
+      puts "Goodbye!".colorize(:light_blue)
     end 
   end
 
@@ -40,8 +43,19 @@ class CLI
         puts "I am terribly sorry, you have entered an invalid number.".colorize(:light_blue)
         puts ""
         self.ask_to_display_more_info
+        
       else 
         self.display_details(number)
+        puts ""
+        puts "Would you like information on another movie? The answer prompts remain the same.".colorize(:light_blue)
+        user_input = gets.strip.downcase
+        
+        if user_input == "yes" || user_input == "y"
+          self.display_info_again
+          
+        else 
+          puts "Glad to have been of service, goodbye!".colorize(:light_blue)
+        end
       end
     end
   end
@@ -59,6 +73,32 @@ class CLI
     puts "Release Date: #{film.release_date}".colorize(:yellow)
     sleep(1)
     puts "Opening Crawl: #{film.opening_crawl}".colorize(:yellow)
+  end
+  
+  def display_info_again
+    self.list_the_films
+    sleep(1)
+    puts "Which film number would you like the details of?".colorize(:light_blue)
+      number = gets.strip.to_i - 1 
+      
+    if !self.valid?(number)
+      puts "I am terribly sorry, you have entered an invalid number.".colorize(:light_blue)
+      puts ""
+      self.ask_to_display_more_info
+        
+    else 
+      self.display_details(number)
+      puts ""
+      puts "Would you like information on another movie? The answer prompts remain the same.".colorize(:light_blue)
+      user_input = gets.strip.downcase
+        
+        if user_input == "yes" || user_input == "y"
+          self.display_info_again
+          
+        else 
+          puts "Glad to have been of service, goodbye!".colorize(:light_blue)
+      end
+    end
   end
   
   
