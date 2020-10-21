5e4d0579dd4cc6da2cdc1a664c5c92ffba99f2fe
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-21 03:08:27 +0000

    Began building get_characters method

--- lib/api.rb
+++ lib/api.rb
@@ -1,9 +1,5 @@
 class API  
 
-  # def self.get_lists 
-  # end  
-
-   
   def self.get_films 
     url = "https://swapi.dev/api/films/" 
     uri = URI(url) 
@@ -14,21 +10,36 @@ class API
     
     
     array_of_films.each do |film_hash|
-      binding.pry 
       film = Film.new
       film.title = film_hash["title"]
       film.episode_id = film_hash["episode_id"]
       film.opening_crawl = film_hash["opening_crawl"]
       film.release_date = film_hash["release_date"]
-      # characters = film_hash["characters"] <- returns an array of links to character APIs
-    end
+      characters_list = film_hash["characters"] 
       
+      # ^- returns an array of links to character APIs
+    end
 
   end 
 
-   
-  def self.get_characters 
-    character_url = "https://swapi.dev/api/people/" 
+  
+  def self.get_characters(url) 
+    url = "https://swapi.dev/api/people/" 
+    uri = URI(url) 
+    response = Net::HTTP.get(uri) 
+    hash = JSON.parse(response) 
+
+    array_of_characters = hash["results"] 
+    
+    
+    array_of_characters.each do |character_hash|
+      
+      character = Character.new
+      character.name = character_hash["name"]
+      character.birth_year = character_hash["birth_year"]
+      # character.home_planet = character_hash["homeworld"] <- returns a link to the home world's api
+      # character.films = character_hash["films"] <- returns an array of links to film apis
+    end
   end 
 
    
--- lib/cli.rb
+++ lib/cli.rb
@@ -4,11 +4,10 @@ class CLI
     puts "Hello! I am F1NDR, your personal Star Wars information finder.".colorize(:blue) 
     puts "" 
     puts "If you'd like to utilize my services, please type 'yes' or 'y'. If you'd like to exit this program, please hit any other key.".colorize(:yellow) 
-    API.get_films 
+    API.get_characters 
     user_input = gets.strip.downcase 
 
   end 
 
-   
 
 end
