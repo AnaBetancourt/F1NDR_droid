0ede0583cebe4ca06372f73c7e43b983eb208fac
Ana Betancourt <anaa.betancourt13@gmail.com>
2020-10-21 04:10:03 +0000

    Commented out character-related coding to focus on building level 1 of my app.

--- lib/api.rb
+++ lib/api.rb
@@ -19,33 +19,32 @@ class API
       film.release_date = film_hash["release_date"]
       # fim.characters = need to get an array of all the characters from the movie and return their names in a list of strings
        
-      characters_list = film_hash["characters"] 
-        characters_list.each do |link| 
-          binding.pry
-          API.make_character(link)
-        end
+      # characters_list = film_hash["characters"] 
+      #   characters_list.each do |link| 
+      #     API.make_character(link)
+      #   end
+        
+        #won't parse link through iteration but will when individually passed in as argument....hmmm
      
     end
 
   end 
 
   
-  def self.make_character(link)
-    url = link 
-    uri = URI(url) 
-    response = Net::HTTP.get(uri) 
-    hash = JSON.parse(response) 
-    array_of_characters = hash["results"] 
+  # def self.make_character(link)
+  #   url = link
+  #   uri = URI(url) 
+  #   response = Net::HTTP.get(uri) 
+  #   character_hash = JSON.parse(response) 
     
-    
-    array_of_characters.each do |character_hash|
-      character = Character.new
-      character.name = character_hash["name"]
-      character.birth_year = character_hash["birth_year"]
-      # character.home_planet = character_hash["homeworld"] <- returns a link to the home world's api
-      # character.films = character_hash["films"] <- returns an array of links to film apis
-    end
-  end 
+  #   if !Character.all.include?(character_hash["name"])
+  #     character = Character.new
+  #     character.name = character_hash["name"]
+  #     character.birth_year = character_hash["birth_year"]
+  #   # character.home_planet = character_hash["homeworld"] <- returns a link to the home world's api
+  #   # character.films = character_hash["films"] <- returns an array of links to film apis
+  #   end
+  # end 
 
    
 end
